ea5a79ec85c29b36d4188891099e40d1124470c8
krishna sai ram <74918581+krishcodz@users.noreply.github.com>
2023-05-19 16:24:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://127.0.0.1:27017/todolistDB", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://krishnax711:Krish007@cluster0.gxbln23.mongodb.net/todolistDB", {useNewUrlParser: true});
 
 const itemSchema = new mongoose.Schema({
   name: String
